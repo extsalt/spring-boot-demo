51d673b02df18216910e130a9775b9017d5e35bf
Ganesh K <ganeshk.work@gmail.com>
2021-09-26 18:55:42 +0530

    fix

--- src/main/kotlin/com/example/demo1/Image/ImageController.kt
+++ src/main/kotlin/com/example/demo1/Image/ImageController.kt
@@ -2,6 +2,9 @@ package com.example.demo1.Image
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import java.awt.BasicStroke
+import java.awt.Color
+import java.awt.Graphics2D
 import java.io.File
 import javax.imageio.ImageIO
 
@@ -12,7 +15,10 @@ class ImageController {
     @GetMapping("/img")
     fun test() {
         val bufferedReader = ImageIO.read(File("C:\\Users\\ganes\\Pictures\\IMG_20210924_162120.jpg"))
-        bufferedReader.graphics.drawRect(0, bufferedReader.height - 20, 80, 80)
+        var graphic = bufferedReader.createGraphics()
+        graphic.color = Color.MAGENTA
+        graphic.stroke = BasicStroke(10F)
+        graphic.drawLine(0, bufferedReader.height - 20, 0,0)
         var file = File("test.jpg")
         ImageIO.write(bufferedReader, "jpg", file)
     }
