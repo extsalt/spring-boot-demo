bbab0ddc93e2be50eedbd91fc567cee898fd5bca
Ganesh K <ganeshk.work@gmail.com>
2021-09-28 07:31:32 +0530

    create empty canvas

--- src/main/kotlin/com/example/demo1/Image/ImageController.kt
+++ src/main/kotlin/com/example/demo1/Image/ImageController.kt
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import java.awt.BasicStroke
 import java.awt.Color
-import java.awt.Graphics2D
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import javax.swing.Spring.height
+
 
 @Controller
 class ImageController {
@@ -14,12 +16,22 @@ class ImageController {
 
     @GetMapping("/img")
     fun test() {
-        val bufferedReader = ImageIO.read(File("C:\\Users\\ganes\\Pictures\\IMG_20210924_162120.jpg"))
-        var graphic = bufferedReader.createGraphics()
-        graphic.color = Color.MAGENTA
-        graphic.stroke = BasicStroke(10F)
-        graphic.drawLine(0, bufferedReader.height - 20, 0,0)
-        var file = File("test.jpg")
-        ImageIO.write(bufferedReader, "jpg", file)
+        this.createCanvas()
+//        val bufferedReader = ImageIO.read(File("C:\\Users\\ganes\\Pictures\\IMG_20210924_162120.jpg"))
+//        var graphic = bufferedReader.createGraphics()
+//        graphic.color = Color.MAGENTA
+//        graphic.stroke = BasicStroke(10F)
+//        graphic.drawLine(0, bufferedReader.height - 20, 0, 0)
+//        var file = File("test.jpg")
+//        ImageIO.write(bufferedReader, "jpg", file)
+    }
+
+    fun createCanvas(width: Int = 640, height: Int = 480) {
+        val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+        val g2d = bufferedImage.createGraphics()
+        g2d.background = Color.WHITE
+        g2d.fillRect(0, 0, width, height)
+        val file = File("test1.jpg")
+        ImageIO.write(bufferedImage, "jpg", file)
     }
 }
